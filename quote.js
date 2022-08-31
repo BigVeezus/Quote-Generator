3f52981f7dd3acf1d0933f776b239e6108480b3d
Elvis Osuji <97410294+BigVeezus@users.noreply.github.com>
2022-08-31 10:38:03 +0100

    fixed buttons and links

--- quote.js
+++ quote.js
@@ -1,6 +1,7 @@
 const quoteText = document.getElementById('text');
 const author = document.getElementById('author');
 const button = document.getElementById('new-quote')
+const tweetButton = document.getElementById('quote-tweet')
 
 
 const url = "https://api.quotable.io/random";
@@ -11,8 +12,14 @@ let getQuote = () => {
       .then((item) => {
         quoteText.innerText = item.content;
         author.innerText = item.author;
+      const currentQuote = quoteText.innerText;
+      const currentAuthor = author.innerText;
+      // console.log(currentQuote)
+      
       });
 };
+
+
 
 window.addEventListener("load", getQuote);
 button.addEventListener("click", getQuote);
